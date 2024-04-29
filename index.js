de59735682a275284c4de915b046966cd6920e19
BambooCode98 <jw.thomas98@yahoo.com>
2024-04-29 18:51:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ let width = window.innerWidth,
     height = window.innerHeight;
 const pi = 3.14159265;
 
-ctx.linWidth = 1;
+ctx.lineWidth = 1;
 ctx.fillStyle = "black";
 
 ctx.beginPath();
-for(let i = 0; i < height; i++) {
+for(let i = 0; i < height; i+=10) {
   for(let j = 0; j < width; j++) {
-    ctx.arc(i,j,1,0,2*pi);    
+    ctx.arc(i,j,1,0,2*pi);  
+    ctx.fillStyle="hsl($i,$j,$i+j);
   }
 }
 ctx.fill();
