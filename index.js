3c0d2bbc4152cd921e4a211e4f2e89c4dec8f92d
James Thomas <jw.thomas98@yahoo.com>
2024-06-27 12:03:47 -0500

    update portfolio

--- index.js
+++ index.js
@@ -3,34 +3,36 @@
 
 
 function main() {
-    let canvas = document.querySelector('.canvas');
+    // let canvas = document.querySelector('.canvas');
+    let html = document.querySelector('html');
+    // html.style.height =screen.height;
+    // let gl = canvas.getContext('webgl');
+    // console.log(window);
+    // console.log(screen);
+    // if(!gl) {
+    //     console.log("Webgl not found. Browser may not support it.");
+    //     return;
+    // }
     
-    let gl = canvas.getContext('webgl');
+    // let width = window.outerWidth,
+    //     height = window.outerHeight;
+    // const pi = 3.14159265;
+    // canvas.width = width;
+    // canvas.height = height;
     
-    if(!gl) {
-        console.log("Webgl not found. Browser may not support it.");
-        return;
-    }
+    // gl.clearColor(0.0,0.0,0.0,1.0);
+    // gl.clear(gl.COLOR_BUFFER_BIT);
     
-    let width = window.outerWidth,
-        height = window.outerHeight;
-    const pi = 3.14159265;
-    canvas.width = width;
-    canvas.height = height;
-    
-    gl.clearColor(0.0,0.0,0.0,1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    
-    let particles = [];
+    // let particles = [];
 
 }
 
-function animate() {
-    gl.fillRect(0,0,width,height);
+// function animate() {
+//     gl.fillRect(0,0,width,height);
 
 
-    requestAnimationFrame(animate);
-}
+//     requestAnimationFrame(animate);
+// }
 
 
 
